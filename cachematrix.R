96aeefdf5ce448b672d3036bbecd7d2cfc39e55b
Wayde Fleener <wcflee@yahoo.com>
2016-03-27 21:51:06 -0500

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix calculates the inverse of the matrix and Caches it
+## cacheSolve checks to see if the matrix changed.  If not, it returns the Cached inverse.  If it has, it uses makeCacheMatrix to calculate the change
 
-## Write a short comment describing this function
+## This caches the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cacheMatrix<<-solve(x)
+  
 }
 
 
-## Write a short comment describing this function
+## This checks if the matrix has changed.  If so, recalculate the inverse matrix. If not, return the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+#Check if an inverse matrix has ever been cached.  If not, then Cache it.
+  if (!exists("m")){
+    m<<-x
+    cacheMatrix<-makeCacheMatrix(x)
+  }
+  
+#Check if matrix has changed  
+  if (m==x){
+#Matrix has not changed so return Cached Matrix
+    return(cacheMatrix)
+    
+  } else {
+#Matrix has changed so call makeCacheMatrix to calculate it
+    cacheMatrix<-makeCacheMatrix(x)
+    m<<-x
+    return(cacheMatrix)
+  }
+  
+  
+  
 }
+
+
+
+
